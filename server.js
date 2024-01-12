ba1a54692527af7df5f1b6919ee7cce026cd237b
parthvirani7 <138418526+parthvirani7@users.noreply.github.com>
2024-01-12 13:44:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const http = require("http");
 const express = require("express");
-const { connectDB } = require("./db/dbconnect");
 const bodyParser = require("body-parser");
+const { connectDB } = require("./db/dbconnect");
 const routes = require("./routes");
 
 const app = express();
@@ -15,5 +15,5 @@ connectDB();
 app.use("/v1", routes);
 
 http.createServer(app).listen(process.env.PORT, () => {
-  console.log("server started");
+  console.log("server started successfully");
 });
