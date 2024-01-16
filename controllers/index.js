86cf10fbf0f24ab929ab71d3286955e5c009ff30
parthvirani7 <138418526+parthvirani7@users.noreply.github.com>
2024-01-16 15:01:58 +0530

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.userController = require("./user.controller");
-module.exports.movieController = require("./movie.controller");
-module.exports.ticketController = require("./ticket.controller");
